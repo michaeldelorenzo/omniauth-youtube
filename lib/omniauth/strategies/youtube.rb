ebba4b8f258675712faee668e852e537075c654e
Khang Pham <vkhang55@gmail.com>
2013-02-12 19:24:19 -0800

    added profile as well

--- lib/omniauth/strategies/youtube.rb
+++ lib/omniauth/strategies/youtube.rb
@@ -14,7 +14,7 @@ module OmniAuth
       }
 
       option :authorize_params, {
-        :scope => 'http://gdata.youtube.com https://www.googleapis.com/auth/userinfo.email',
+        :scope => 'http://gdata.youtube.com https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
         :access_type => 'offline'
       }
 
